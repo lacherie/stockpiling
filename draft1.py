6080689ba07cba66f3e186ace1fda7bcbbcb44dc
Nadia Montazeri <nadiamirmontazeri@gmail.com>
2023-04-24 15:51:48 +0200

    Format

--- draft1.py
+++ draft1.py
@@ -1,6 +1,6 @@
-
 from dataclasses import dataclass
 from typing import Dict, List, Tuple
+
 
 @dataclass(frozen=True)
 class Box:
@@ -8,9 +8,9 @@ class Box:
     len: float
     wid: float
     dep: float
-    
-    def volume(self) -> float: 
-        return(self.len * self.wid * self.dep)
+
+    def volume(self) -> float:
+        return self.len * self.wid * self.dep
 
 
 @dataclass(frozen=True)
@@ -18,13 +18,17 @@ class Item:
     name: str
     single_price: float
     box: Box
-    accessories: List[Tuple["Item", int]]
+    accessories: List[Tuple["Item", int]] = []
 
     def total_price(self) -> float:
-        subprices = [accessory.total_price() * amount_needed for accessory, amount_needed in self.accessories]
+        subprices = [
+            accessory.total_price() * amount_needed
+            for accessory, amount_needed in self.accessories
+        ]
         all_prices = subprices + [self.single_price]
         return sum(all_prices)
-    
+
+
 @dataclass(frozen=True)
 class Pandemic:
     name: str
@@ -38,137 +42,159 @@ class Pandemic:
     average_days_in_hospital_icu: float
 
     def infected_total(self, population: int) -> float:
-        return(self.infected_per_population * population)
-    
+        return self.infected_per_population * population
+
     def seek_healthcare_total(self, population: int) -> float:
-        return(self.seek_healthcare_per_infected * self.infected_total(population))
-    
+        return self.seek_healthcare_per_infected * self.infected_total(population)
+
     def hospitalised_total(self, population: int) -> float:
-        return(self.hospitalised_per_seek_healthcare * self.seek_healthcare_total(population))
-    
+        return self.hospitalised_per_seek_healthcare * self.seek_healthcare_total(
+            population
+        )
+
     def icu_cases_total(self, population: int) -> float:
-        return(self.icu_case_per_hospitalised * self.hospitalised_total(population))
-    
+        return self.icu_case_per_hospitalised * self.hospitalised_total(population)
+
     def ventilated_total(self, population: int) -> float:
-        return(self.ventilated_per_icu_case * self.icu_cases_total(population))
-    
+        return self.ventilated_per_icu_case * self.icu_cases_total(population)
+
     def deaths(self, population: int) -> float:
-        return(self.fatality * self.infected_total(population))
+        return self.fatality * self.infected_total(population)
 
     def outpatient_visits(self, population: int) -> float:
-        return(self.seek_healthcare_total(population) - self.hospitalised_total(population))
-    
-    def non_icu_patient_days(self, population: int) -> float:
-        return(self.hospitalised_total(population)* self.average_days_in_hospital_not_icu)
-    
-    def non_mv_icu_patient_days(self, population: int) -> float:
-        return(self.icu_cases_total(population) * (1-self.ventilated_per_icu_case)* self.average_days_in_hospital_icu)
-    
-    def mv_icu_patient_days(self, population: int) -> float:
-        return(self.ventilated_total(population) * self.average_days_in_hospital_icu)
-    
-    def summary_of_contacts(self, population: int) -> List[float]:
-        list0=[self.hospitalised_total(population),
-               self.outpatient_visits(population),
-               self.non_icu_patient_days(population),
-               self.non_mv_icu_patient_days(population),
-               self.mv_icu_patient_days(population)]
-        return(list0)
-    
-    def dictionary_of_contacts(self,Profession,population) -> Dict{}:
-        dictionary0 = {self.hospitalised_total(population),
-               self.outpatient_visits(population),
-               self.non_icu_patient_days(population),
-               self.non_mv_icu_patient_days(population),
-               self.mv_icu_patient_days(population)
+        return self.seek_healthcare_total(population) - self.hospitalised_total(
+            population
+        )
 
+    def non_icu_patient_days(self, population: int) -> float:
+        return (
+            self.hospitalised_total(population) * self.average_days_in_hospital_not_icu
+        )
+
+    def non_mv_icu_patient_days(self, population: int) -> float:
+        return (
+            self.icu_cases_total(population)
+            * (1 - self.ventilated_per_icu_case)
+            * self.average_days_in_hospital_icu
+        )
+
+    def mv_icu_patient_days(self, population: int) -> float:
+        return self.ventilated_total(population) * self.average_days_in_hospital_icu
+
+    def summary_of_contacts(self, population: int) -> List[float]:
+        list0 = [
+            self.hospitalised_total(population),
+            self.outpatient_visits(population),
+            self.non_icu_patient_days(population),
+            self.non_mv_icu_patient_days(population),
+            self.mv_icu_patient_days(population),
+        ]
+        return list0
+
+    def dictionary_of_contacts(self, Profession, population) -> Dict["Profession", int]:
+        dictionary0 = {
+            self.hospitalised_total(population),
+            self.outpatient_visits(population),
+            self.non_icu_patient_days(population),
+            self.non_mv_icu_patient_days(population),
+            self.mv_icu_patient_days(population),
         }
 
 
 @dataclass
 class Profession:
     name: str
-    contacts_per_hospitalised: int #0 for other HCW, only input for escort
-    contacts_per_outpatient_visit: int #0 for resp tech, radio tech, housekeepers 
-    contacts_per_non_icu_patient_day: int #0 for administrative, only input for phlebotomists
-    contacts_per_icu_patient_day: int #0 for administrative
-    contacts_per_mv_day: int #0 for administrative
+    contacts_per_hospitalised: int = 0
+    """0 for Other HCW, only input for Escort"""
+    contacts_per_outpatient_visit: int = 0  # 0 for resp tech, radio tech, housekeepers
+    contacts_per_non_icu_patient_day: int = (
+        0  # 0 for administrative, only input for phlebotomists
+    )
+    contacts_per_icu_patient_day: int = 0  # 0 for administrative
+    contacts_per_mv_day: int = 0  # 0 for administrative
 
-    def total_contacts(self,Pandemic,population)-> int:
-        patient_contacts = Pandemic.summary_of_contacts(Pandemic,population)
-        profession_contacts = 
+    def total_contacts(self, Pandemic, population) -> int:
+        patient_contacts = Pandemic.summary_of_contacts(Pandemic, population)
+        profession_contacts = 0
 
-
- 
 
 if __name__ == "__main__":
     influenza1918 = Pandemic(
-        name= "Influenza 1918",
+        name="Influenza 1918",
         infected_per_population=0.3,
         seek_healthcare_per_infected=0.5,
         hospitalised_per_seek_healthcare=0.22,
         icu_case_per_hospitalised=0.15,
         ventilated_per_icu_case=0.5,
-        fatality=0.021
+        fatality=0.021,
     )
 
-if __name__ == "__main__":
     md = Profession(
         name="MD",
         contacts_per_hospitalised=3,
         contacts_per_outpatient_visit=1,
         contacts_per_non_icu_patient_day=2,
         contacts_per_icu_patient_day=4,
-        contacts_per_mv_day=4
+        contacts_per_mv_day=4,
     )
+
     rn = Profession(
         name="RN",
         contacts_per_hospitalised=5,
         contacts_per_outpatient_visit=2,
         contacts_per_non_icu_patient_day=6,
         contacts_per_icu_patient_day=24,
-        contacts_per_mv_day=24
+        contacts_per_mv_day=24,
     )
 
-        
-        
-if __name__ == "__main__":
     papr = Item(
         name="PAPR",
-        single_price=500.0, 
-        box = Box(
-            items_per_box = 1,
-            len=20.0, wid=16.0, dep=10.0
-        ),
+        single_price=500.0,
+        box=Box(items_per_box=1, len=20.0, wid=16.0, dep=10.0),
         accessories=[
-            (Item(
-                name="Filter set",
-                single_price=27.13,
-                box = Box(items_per_box = 3,len=9.0,wid=9.0,dep=6.0),
-                accessories=[]
-            ),3),
-            (Item(name="Battery",
-                 single_price=286.0,
-                 box = Box(1,10.0,10.0,9.0),
-                 accessories=[]
-            ),1),
-            (Item("Hood",30.87,Box(3,9.0,9.0,9.0),accessories=[]
-            ),3),
-            (Item("Tube",30.89,Box(5,20.0,16.0,10.0),accessories=[]
-            ),3)
-        ]
+            (
+                Item(
+                    name="Filter set",
+                    single_price=27.13,
+                    box=Box(items_per_box=3, len=9.0, wid=9.0, dep=6.0),
+                    accessories=[],
+                ),
+                3,
+            ),
+            (
+                Item(
+                    name="Battery",
+                    single_price=286.0,
+                    box=Box(1, 10.0, 10.0, 9.0),
+                    accessories=[],
+                ),
+                1,
+            ),
+            (Item("Hood", 30.87, Box(3, 9.0, 9.0, 9.0)), 3),
+            (Item("Tube", 30.89, Box(5, 20.0, 16.0, 10.0)), 3),
+        ],
     )
     elastomeric = Item(
         name="Elastomeric",
         single_price=25.0,
-        box=Box(items_per_box = 10,len=7.0,wid=13.0,dep=18.0),
-        accessories=[(Item(name="E Filter set", single_price=2.5, box= Box(1,5.3,5.1,1.4), accessories=[]),3)]
+        box=Box(items_per_box=10, len=7.0, wid=13.0, dep=18.0),
+        accessories=[
+            (
+                Item(
+                    name="E Filter set",
+                    single_price=2.5,
+                    box=Box(1, 5.3, 5.1, 1.4),
+                    accessories=[],
+                ),
+                3,
+            )
+        ],
     )
     n95 = Item(
         name="N95",
         single_price=0.25,
-        box=Box(items_per_box=20,len=12.0,wid=6.0,dep=6.0),
-        accessories=[]
+        box=Box(items_per_box=20, len=12.0, wid=6.0, dep=6.0),
+        accessories=[],
     )
 
 
